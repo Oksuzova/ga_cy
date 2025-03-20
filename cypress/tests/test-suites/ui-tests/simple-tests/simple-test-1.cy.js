9a61d474fd694d5dcdaf56e3acd4388b10a2e386
Oksuzova <oksuzova@gmail.com>
2025-03-21 00:28:03 +0200

    add gh-pages

--- cypress/tests/test-suites/ui-tests/simple-tests/simple-test-1.cy.js
+++ cypress/tests/test-suites/ui-tests/simple-tests/simple-test-1.cy.js
@@ -8,7 +8,7 @@ describe('Simple Test Suite 1', () => {
     it('always passes - with wait', () => {
         allure.step("Wait for 500ms and assert", () => {
             cy.wait(500);
-            expect(5 > 3).to.be.true;
+            expect(5 > 6).to.be.true;
         });
     });
 });
