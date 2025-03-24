89f799f4c8111eb63e4a4d2e4d9e4daa06ffd601
Oksuzova <oksuzova@gmail.com>
2025-03-25 01:07:13 +0200

    add multiple tests run by tests suite

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,19 +1,3 @@
-import '@shelex/cypress-allure-plugin';
+import "allure-cypress";
 
-beforeEach(function() {
-    try {
-        const filePath = Cypress.spec.relative;
-        let suiteFolder = 'unknown';
-
-        if (filePath && filePath.includes('/')) {
-            suiteFolder = filePath.split('/').slice(-2)[0];
-        }
-
-        cy.allure()
-            .epic(suiteFolder)
-            .feature(this.currentTest.parent.title)
-            .story(`${this.currentTest.parent.title} [${suiteFolder}]`);
-    } catch (e) {
-        console.error('Error setting Allure labels:', e);
-    }
-});
+console.log('Support file loaded');
